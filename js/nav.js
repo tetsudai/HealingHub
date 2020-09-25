587d0e8289673629a50ea0cfc2e94560d2c1b2a1
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-09-25 21:24:08 +1000

    Navbar burger fix

--- js/nav.js
+++ js/nav.js
@@ -10,33 +10,11 @@ Project     ::  Official website for Healing Hub Inc, a peer-to-peer mental heal
 GitHub      ::  https://github.com/tetsudai/HealingHub
 License     ::  MIT License 2020
 
-*stolen from the spec implementation at
-https://bulma.io/documentation/components/navbar/
-
 */
 
-document.addEventListener('DOMContentLoaded', () => {
-
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
-
-});
+function navclick() {
+  var button = document.getElementById("navbarBurger");
+  button.classList.toggle("is-active");
+  var menu = document.getElementById("navbarTarget");
+  menu.classList.toggle("is-active");
+};
